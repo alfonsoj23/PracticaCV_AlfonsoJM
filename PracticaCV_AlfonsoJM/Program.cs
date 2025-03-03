ce0258a8b21ddaaf974deb5afc4b1394e72355d5
Alfonso Jimenez <Alfonso Jimenez@ALFONSOJ>
2025-03-03 20:44:49 +0100

    Agregar archivos de proyecto.

---
+++ PracticaCV_AlfonsoJM/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PracticaCV_AlfonsoJM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
